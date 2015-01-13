86e51f34d17561ab4801d6121b61e4abe5b482c5
Collin Anderson <cmawebsite@gmail.com>
2015-01-13 11:44:02 -0500

    the problem

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Household
 
 # Register your models here.
+@admin.register(Household)
+class PersonAdmin(admin.ModelAdmin):
+    list_filter = ['studenthousehold__primary']
--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+
+class Household(models.Model):
+    students = models.ManyToManyField(Student, through='StudentHousehold')
+
+class StudentHousehold(models.Model):
+    household = models.ForeignKey(Household)
+    student = models.ForeignKey(Student)
+    primary = models.BooleanField(default=False)
